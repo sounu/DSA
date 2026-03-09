b97a8852c4299f2afacf98530e2f3fde49f09626
sounu <tanmaysounu23@gmail.com>
2026-03-09 23:23:55 +0530

    181. Employees Earning More Than Their Managers

---
+++ Sql/test.sql
@@ -0,0 +1,10 @@
+-- # Write your MySQL query statement below
+-- 181. Employees Earning More Than Their Managers
+SELECT
+    e.name AS Employee
+FROM
+    Employee e
+JOIN
+    Employee m ON e.managerId = m.id
+WHERE
+    e.salary > m.salary;
